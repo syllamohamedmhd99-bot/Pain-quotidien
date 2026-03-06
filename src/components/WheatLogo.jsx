f48d32f369ceea87d0023d80a3997870234332c3
Mohamed Sekou Sylla <mohamed@example.com>
2026-03-06 21:35:31 +0000

    correction

--- src/components/WheatLogo.jsx
+++ src/components/WheatLogo.jsx
@@ -5,9 +5,11 @@ import './WheatLogo.css';
 export default function WheatLogo({ size = 32, className = "" }) {
     return (
         <div className={`wheat-logo-container ${className}`} style={{ width: size, height: size }}>
-            <Wheat size={size * 0.75} className="wheat-stalk stalk-1" />
-            <Wheat size={size * 1} className="wheat-stalk stalk-2" />
-            <Wheat size={size * 0.75} className="wheat-stalk stalk-3" />
+            <div className="wheat-bouquet">
+                <Wheat size={size * 0.75} className="wheat-stalk stalk-1" />
+                <Wheat size={size * 1} className="wheat-stalk stalk-2" />
+                <Wheat size={size * 0.75} className="wheat-stalk stalk-3" />
+            </div>
         </div>
     );
 }
