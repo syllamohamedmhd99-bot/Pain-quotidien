8b4ec3456ce16d7a6b840322f65b0e011c053e25
Mohamed Sekou Sylla <mohamed@example.com>
2026-03-07 21:53:39 +0000

    correction

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { ShoppingBag, TrendingUp, Wheat, Users, Truck, AlertTriangle, ArrowRight, DollarSign } from 'lucide-react';
+import { ShoppingBag, TrendingUp, Wheat, Users, Truck, AlertTriangle, ArrowRight, DollarSign, Package } from 'lucide-react';
 import { useState, useEffect } from 'react';
 import { supabase } from '../supabaseClient';
 import './Dashboard.css';
